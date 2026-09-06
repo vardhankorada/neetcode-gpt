ecb94428475d88ce6bce817335d7955e7d9cca3c
vardhankorada <35251469+vardhankorada@users.noreply.github.com>
2026-09-06 12:14:49 +0530

    Update Word embeddings (NLP 1/4)

--- model/embeddings.py
+++ model/embeddings.py
@@ -4,9 +4,4 @@ from numpy.typing import NDArray
 
 class Solution:
     def lookup(self, embeddings: NDArray[np.float64], token_ids: NDArray[np.int64]) -> NDArray[np.float64]:
-        # embeddings: (vocab_size, embed_dim) matrix
-        # token_ids: 1D array of integer token IDs
-        # Return the embedding vectors for the given token IDs
-        # return np.round(your_answer, 5)
-        # print(embeddings[token_ids])
         return np.round(embeddings[token_ids],5)
