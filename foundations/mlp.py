da6a007c32477eba4b2262afe74aed9fb0e3ab31
vardhankorada <35251469+vardhankorada@users.noreply.github.com>
2026-09-07 20:52:56 +0530

    Update MLP from scratch (Build a Neural Net 4/5)

--- foundations/mlp.py
+++ foundations/mlp.py
@@ -5,16 +5,11 @@ from typing import List
 
 class Solution:
     def forward(self, x: NDArray[np.float64], weights: List[NDArray[np.float64]], biases: List[NDArray[np.float64]]) -> NDArray[np.float64]:
-        # x: 1D input array
-        # weights: list of 2D weight matrices
-        # biases: list of 1D bias vectors
-        # Apply ReLU after each hidden layer, no activation on output layer
-        # return np.round(your_answer, 5)
         h = x
         for ind in range(0,len(weights)):
             z = self.linear(weights[ind],biases[ind],h)
             if ind < len(weights)-1: h = self.relu(z)
-        return z
+        return np.round(z,5)
 
     def relu(self,z): return np.maximum(0,z)
 
