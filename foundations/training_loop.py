89a2df6794648f9e06082c2422b0f2ac479db4a8
vardhankorada <35251469+vardhankorada@users.noreply.github.com>
2026-09-20 22:24:53 +0530

    Update Training loop (Training 1/4)

--- foundations/training_loop.py
+++ foundations/training_loop.py
@@ -8,7 +8,6 @@ class Solution:
         w,b = np.zeros(X.shape[1]),0
         for epoch_idx in range(epochs):
             y_pred = self.get_res(X,w,b)
-            loss = self.get_loss(y,y_pred)
             grad = self.get_grad(X,y,y_pred)
             w,b = w - lr * grad[0], b - lr * grad[1]
         return (np.round(w,5), round(b,5))
@@ -18,6 +17,3 @@ class Solution:
         return (2/n * np.matmul(X.T,(y_pred-y))),(2/n * np.sum(y_pred-y))
 
     def get_res(self,X,w,b): return np.matmul(X,w)+b
-
-    def get_loss(self,y,y_pred): return np.mean(np.pow(y_pred-y,2))
-
